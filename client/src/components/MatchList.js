3bbe19c508052ccbfef49ec4438d177e777f366f
Angus Warman <anguswarman+github@gmail.com>
2019-08-29 15:59:42 +0800

    Update MatchList.js

--- client/src/components/MatchList.js
+++ client/src/components/MatchList.js
@@ -11,8 +11,41 @@ class MatchList extends Component {
 					sidebarVisible={this.props.sidebarVisible}
 					handleSidebarClick={this.props.handleSidebarClick}
 				/>
-				<div id="container" style={{height:"100vh"}}>
-					Content goes here
+				<div id="container" style={{height:"100vh",width:"90"}}>
+					<div class="ui form">
+						<div class="field">
+							<div class="ui labeled input">
+								<div class="ui label">
+									Search
+								</div>
+								<input type="text" />
+							</div>
+						</div>
+						<div class="three fields">
+							<div class="field">
+								<div class="ui labeled input">
+									<div class="ui label">
+										Between
+									</div>
+									<input type="text" placeholder="Start date" />
+								</div>
+							</div>
+							<div class="field">
+								<div class="ui input">
+									<input type="text" placeholder="End date" />
+								</div>
+							</div>
+							<div class="field">
+								<div class="ui labeled input">
+									<div class="ui label">
+										League
+									</div>
+									<input type="text" />
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="results"></div>
 				</div>
 			</div>
 		);
