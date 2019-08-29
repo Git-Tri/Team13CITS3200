ab4d366a03a47c36d0188edf0a8095419e4360fc
Angus Warman <anguswarman+github@gmail.com>
2019-08-29 22:02:19 +0800

    Update MatchList.js

--- client/src/components/MatchList.js
+++ client/src/components/MatchList.js
@@ -11,14 +11,14 @@ class MatchList extends Component {
 					sidebarVisible={this.props.sidebarVisible}
 					handleSidebarClick={this.props.handleSidebarClick}
 				/>
-				<div id="container" style={{height:"100vh",width:"90"}}>
-					<div class="ui form">
+				<div id="container" style={{height:"100vh"}}>
+					<div class="ui form" method="POST" action="/search">
 						<div class="field">
 							<div class="ui labeled input">
 								<div class="ui label">
 									Search
 								</div>
-								<input type="text" />
+								<input type="text" name="search-name"/>
 							</div>
 						</div>
 						<div class="three fields">
@@ -27,12 +27,12 @@ class MatchList extends Component {
 									<div class="ui label">
 										Between
 									</div>
-									<input type="text" placeholder="Start date" />
+									<input type="date" name="search-startdate" placeholder="Start date" />
 								</div>
 							</div>
 							<div class="field">
 								<div class="ui input">
-									<input type="text" placeholder="End date" />
+									<input type="date" name="search-enddate" placeholder="End date" />
 								</div>
 							</div>
 							<div class="field">
@@ -40,10 +40,11 @@ class MatchList extends Component {
 									<div class="ui label">
 										League
 									</div>
-									<input type="text" />
+									<input type="text" name="search-league"/>
 								</div>
 							</div>
 						</div>
+						<button class="ui button" value="submit" style={{width:"100vh"}}>Submit</button>
 					</div>
 					<div class="results"></div>
 				</div>
