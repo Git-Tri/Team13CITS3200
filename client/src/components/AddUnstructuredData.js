b1eac02072b3c395deda49e1ecc005ec00c1daa9
garchomprokz <garchomprokz@yahoo.com>
2019-08-31 22:57:36 +0800

    Add unstructered data and unstructured data list drafts
    Elements put into their rough locations based on wireframes. No functionality.

--- client/src/components/AddUnstructuredData.js
+++ client/src/components/AddUnstructuredData.js
@@ -11,8 +11,106 @@ class AddUnstructuredData extends Component {
 					sidebarVisible={this.props.sidebarVisible}
 					handleSidebarClick={this.props.handleSidebarClick}
 				/>
-				<div id="container" style={{height:"100vh"}}>
-					Content goes here
+                <div id="container" style={{ height: "100vh" }}>
+                    <div class="ui form">
+                        <div class="inline fields">
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Back
+                                </button>
+                            </div>
+                            <div class="ten wide field">
+                                <input type="text" placeholder="Source, Team V Team, Date"/>
+                            </div>
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Logout
+                                </button>
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Edit
+                                </button>
+                            </div>
+                            <div class="ten wide field">
+                            </div>
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Toggle: Edited
+                                </button>
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="eleven wide field">
+                                <label>
+                                    Title
+                                </label>
+                                <input type="text"/>
+                            </div>
+                            <div class="five wide field">
+                                <label>
+                                    Author
+                                </label>
+                                <input type="text"/>
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="five wide field">
+                                <label>
+                                    Published
+                                </label>
+                                <input type="text" placeholder ="Date"/>
+                            </div>
+                            <div class="six wide field">
+                                <label>
+                                    Extracted
+                                </label>
+                                <input type="text" placeholder="Date" />
+                            </div>
+                            <div class="five wide field">
+                                <label>
+                                    URL
+                                </label>
+                                <input type="text" />
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="five wide field">
+                                <div class="ui pointing below label">
+                                    Content
+                                </div>
+                            </div>
+                            <div class="eight wide field">
+                                <label>
+                                    Match
+                                </label>
+                                <input type="text"/>
+                            </div>
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Change
+                                </button>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <textarea></textarea>
+                        </div>
+                        <div class="inline fields">
+                            <div class="ten wide field" />
+                            <div class="three wide field">
+                                <button class="ui red button">
+                                    Delete
+                                </button>
+                            </div>
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Save
+                                </button>
+                            </div>
+                        </div>
+                    </div>
 				</div>
 			</div>
 		);
--- client/src/components/UnstructuredDataList.js
+++ client/src/components/UnstructuredDataList.js
@@ -12,7 +12,60 @@ class UnstructuredDataList extends Component {
 					handleSidebarClick={this.props.handleSidebarClick}
 				/>
 				<div id="container" style={{height:"100vh"}}>
-					Content goes here
+                    <form class="ui form">
+                        <div class="inline fields">
+                            <div class="three wide field" />
+                            <div class="ten wide field"/>
+                            <div class="three wide field">
+                                <button class="ui primary button">
+                                    Logout
+                                </button>
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="sixteen wide field">
+                                <label>
+                                    Search
+                                </label>
+                                <div class="ui icon input">
+                                    <input type="text"/>
+                                        <i class="search icon"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="eight wide field">
+                                <label>
+                                    Between
+                                </label>
+                                <input type="text" placeholder="Start Date" />
+                                <input type="text" placeholder="End Date" />
+                            </div>
+                            <div class="eight wide field">
+                                <label>
+                                    Match
+                                </label>
+                                <input type="text" />
+                            </div>
+                        </div>
+                        <div class="inline fields">
+                            <div class="eight wide field" />
+                            <div class="eight wide field">
+                                <label>
+                                    League
+                                </label>
+                                <input type="text"/>
+                            </div>
+                        </div>
+                        <div class="sixteen wide field">
+                            <div class="ui message">
+                                <div class="header">
+                                    Search Results
+                                 </div>
+                                <p>To add: list of unstructured data that meets above criteria, featuring date, teams, league/tournament, title and publisher. Clicking on an item should take you to the unstructured data page.</p>
+                            </div>
+                        </div>
+                    </form>
 				</div>
 			</div>
 		);
