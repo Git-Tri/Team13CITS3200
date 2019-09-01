7658383dd9ace2af88b1362f93d906fd2a43df82
Alex Burns <alexanderburns11@yahoo.com>
2019-09-01 17:54:30 +0800

    Added api file to server and basic football api access for testing

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const routes = require("./routes.js");
-const dbAccess = require("./databaseAccess")
+const dbAccess = require("./databaseAccess");
+const api = require("./api");
 
 //start app
 const app = express();
@@ -37,6 +38,8 @@ function getPort()
   }
 
 }
+
+api.api();
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
