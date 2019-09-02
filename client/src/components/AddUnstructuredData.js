5c6948064802ceb21f17f9fde92b6fd43b1e4d1f
garchomprokz <garchomprokz@yahoo.com>
2019-09-02 21:33:09 +0800

    add unstructured data basic functionality
    back button has basic functionality, submit button sends a post request to nowhere

--- client/src/components/AddUnstructuredData.js
+++ client/src/components/AddUnstructuredData.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import PageHeader from './PageHeader.js';
 
 class AddUnstructuredData extends Component {
+    constructor(props) {
+        super(props)
 
-	render() {
+        this.state = {
+            title: '',
+            author: '',
+            published: '',
+            extracted: '',
+            url: '',
+            match: '',
+            content: ''
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        var request = new XMLHttpRequest()
+        request.open('POST', '/add_unstructured_data', true)
+        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+        request.send(JSON.stringify(this.state))
+    }
+
+    goBack = () => {
+        //TODO add confirmation for going back
+        window.history.back()
+    }
+    render() {
+        const { source, date, team1, team2, title, author, published, extracted, url, match, content } = this.state
 		return (
 			<div className="page">
 				<PageHeader 
@@ -12,105 +43,117 @@ class AddUnstructuredData extends Component {
 					handleSidebarClick={this.props.handleSidebarClick}
 				/>
                 <div id="container" style={{ height: "100vh" }}>
-                    <div class="ui form">
+                    <form class="ui form" onSubmit={this.submitHandler}>
                         <div class="inline fields">
-                            <div class="three wide field">
-                                <button class="ui primary button">
+                            <div class="one wide field"/>
+                            <div class="two wide field">
+                                <button onClick={this.goBack} class="fluid ui primary button">
                                     Back
                                 </button>
                             </div>
                             <div class="ten wide field">
-                                <input type="text" placeholder="Source, Team V Team, Date"/>
                             </div>
-                            <div class="three wide field">
-                                <button class="ui primary button">
+                            <div class="two wide field">
+                                <button class="fluid ui primary button">
                                     Logout
                                 </button>
                             </div>
                         </div>
                         <div class="inline fields">
-                            <div class="three wide field">
-                                <button class="ui primary button">
+                            <div class="one wide field"/>
+                            <div class="two wide field">
+                                <button class="fluid ui primary button">
                                     Edit
                                 </button>
                             </div>
-                            <div class="ten wide field">
+                            <div class="three wide field"/>
+                            <div class="four wide field">
+                                <h1>
+                                    New Unstructured Data (or a proper header if available)
+                                </h1>
                             </div>
-                            <div class="three wide field">
-                                <button class="ui primary button">
+                            <div class="three wide field" />
+                            <div class="two wide field">
+                                <button class="fluid ui primary button">
                                     Toggle: Edited
                                 </button>
                             </div>
                         </div>
                         <div class="inline fields">
-                            <div class="eleven wide field">
+                            <div class="one wide field"/>
+                            <div class="ten wide field">
                                 <label>
                                     Title
                                 </label>
-                                <input type="text"/>
+                                <input type="text" name="title" value={title} onChange={this.changeHandler} />
                             </div>
-                            <div class="five wide field">
+                            <div class="four wide field">
                                 <label>
                                     Author
                                 </label>
-                                <input type="text"/>
+                                <input type="text" name="author" value={author} onChange={this.changeHandler} />
                             </div>
                         </div>
                         <div class="inline fields">
+                            <div class="one wide field"/>
                             <div class="five wide field">
                                 <label>
                                     Published
                                 </label>
-                                <input type="text" placeholder ="Date"/>
-                            </div>
-                            <div class="six wide field">
-                                <label>
-                                    Extracted
-                                </label>
-                                <input type="text" placeholder="Date" />
+                                <input type="text" name="published" value={published} onChange={this.changeHandler} placeholder ="Date"/>
                             </div>
                             <div class="five wide field">
                                 <label>
+                                    Extracted
+                                </label>
+                                <input type="text" name="extracted" value={extracted} onChange={this.changeHandler} placeholder="Date" />
+                            </div>
+                            <div class="four wide field">
+                                <label>
                                     URL
                                 </label>
-                                <input type="text" />
+                                <input type="text" name="url" value={url} onChange={this.changeHandler} />
                             </div>
                         </div>
                         <div class="inline fields">
+                            <div class="one wide field"/>
                             <div class="five wide field">
                                 <div class="ui pointing below label">
                                     Content
                                 </div>
                             </div>
-                            <div class="eight wide field">
+                            <div class="seven wide field">
                                 <label>
                                     Match
                                 </label>
-                                <input type="text"/>
+                                <input type="text" name="match" value={match} onChange={this.changeHandler} />
                             </div>
-                            <div class="three wide field">
-                                <button class="ui primary button">
+                            <div class="two wide field">
+                                <button class="fluid ui primary button">
                                     Change
                                 </button>
                             </div>
                         </div>
-                        <div class="field">
-                            <textarea></textarea>
+                        <div class="inline fields">
+                            <div class="one wide field"/>
+                            <div class="fourteen wide field">
+                                <textarea name="content" value={content} onChange={this.changeHandler} />
+                            </div>
                         </div>
                         <div class="inline fields">
-                            <div class="ten wide field" />
-                            <div class="three wide field">
-                                <button class="ui red button">
+                            <div class="eleven wide field" />
+                            <div class="two wide field">
+                                <button class="fluid ui red button">
                                     Delete
                                 </button>
                             </div>
-                            <div class="three wide field">
-                                <button class="ui primary button">
+                            <div class="two wide field">
+                                <button type="submit" class="fluid ui primary button">
                                     Save
                                 </button>
                             </div>
                         </div>
-                    </div>
+                    </form>
 				</div>
 			</div>
 		);
--- client/src/components/UnstructuredDataList.js
+++ client/src/components/UnstructuredDataList.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import PageHeader from './PageHeader.js';
 
 class UnstructuredDataList extends Component {
+    constructor(props) {
+        super(props)
 
-	render() {
+        this.state = {
+            search: '',
+            start: '',
+            end: '',
+            match: '',
+            league: ''
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    render() {
+        const { search, start, end, match, league } = this.state
 		return (
 			<div className="page">
 				<PageHeader 
@@ -14,55 +30,59 @@ class UnstructuredDataList extends Component {
 				<div id="container" style={{height:"100vh"}}>
                     <form class="ui form">
                         <div class="inline fields">
-                            <div class="three wide field" />
-                            <div class="ten wide field"/>
-                            <div class="three wide field">
+                            <div class="thirteen wide field"/>
+                            <div class="two wide field">
                                 <button class="ui primary button">
                                     Logout
                                 </button>
                             </div>
                         </div>
                         <div class="inline fields">
-                            <div class="sixteen wide field">
+                            <div class="one wide field"/>
+                            <div class="fourteen wide field">
                                 <label>
                                     Search
                                 </label>
                                 <div class="ui icon input">
-                                    <input type="text"/>
+                                    <input type="text" name="search" value={search} onChange={this.changeHandler} />
                                         <i class="search icon"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="inline fields">
+                            <div class="one wide field"/>
                             <div class="eight wide field">
                                 <label>
                                     Between
                                 </label>
-                                <input type="text" placeholder="Start Date" />
-                                <input type="text" placeholder="End Date" />
+                                <input type="text" name="start" value={start} onchange={this.changeHandler} placeholder="Start Date" />
+                                <input type="text" name="end" value={end} onchange={this.changeHandler} placeholder = "End Date" />
                             </div>
-                            <div class="eight wide field">
+                            <div class="six wide field">
                                 <label>
                                     Match
                                 </label>
-                                <input type="text" />
+                                <input type="text" name="match" value={match} onChange={this.changeHandler} />
                             </div>
                         </div>
                         <div class="inline fields">
-                            <div class="eight wide field" />
-                            <div class="eight wide field">
+                            <div class="nine wide field" />
+                            <div class="six wide field">
                                 <label>
                                     League
                                 </label>
-                                <input type="text"/>
+                                <input type="text" name="league" value={league} onChange={this.changeHandler} />
                             </div>
                         </div>
-                        <div class="sixteen wide field">
-                            <div class="ui message">
-                                <div class="header">
-                                    Search Results
-                                 </div>
-                                <p>To add: list of unstructured data that meets above criteria, featuring date, teams, league/tournament, title and publisher. Clicking on an item should take you to the unstructured data page.</p>
+                        <div class="inline fields" >
+                            <div class="one wide field"/>
+                            <div class="fourteen wide field">
+                                <div class="ui message">
+                                    <div class="header">
+                                        Search Results
+                                    </div>
+                                    <p>To add: list of unstructured data that meets above criteria, featuring date, teams, league/tournament, title and publisher. Clicking on an item should take you to the unstructured data page.</p>
+                                </div>
                             </div>
                         </div>
                     </form>
