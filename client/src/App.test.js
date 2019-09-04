683d598fcb47a5b088b77d91f988cd19ef0c1aa3
Angus Warman <anguswarman+github@gmail.com>
2019-09-04 18:54:02 +0800

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,21 +4,32 @@ import App from './App';
 import PageHeader from './components/PageHeader'
 import MatchList from './components/MatchList'
 import { BrowserRouter } from 'react-router-dom'
+import { act, Simulate } from 'react-dom/test-utils'
 
-it('app render', () => {
+it('should render the app', () => {
   const router = document.createElement('BrowserRouter');
   ReactDOM.render(<App />, router);
   ReactDOM.unmountComponentAtNode(router);
 });
 
-it('header renders', () => {
+it('should render the header', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<PageHeader />,div);
 	ReactDOM.unmountComponentAtNode(div);
 })
 
-it('MatchList renders', () => {
+it('should render MatchList', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<MatchList />,div);
 	ReactDOM.unmountComponentAtNode(div);
 })
+
+// it('check MatchList user inputs',() => {
+//     const div = document.createElement('div');
+// 	act(() => {
+// 		ReactDOM.render(<MatchList/>,div);
+// 	});
+// 	Simulate('handleChange', { target: {searchtext: 'Text'} });
+// 	expect(onChange).toHaveBeenCalledWith('Text');
+// 	ReactDOM.unmountComponentAtNode(div);
+// })
