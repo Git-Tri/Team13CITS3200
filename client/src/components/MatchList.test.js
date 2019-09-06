2953ff6334cebf712dda52c5678e45f000692fb8
Angus Warman <anguswarman+github@gmail.com>
2019-09-06 10:45:32 +0800

    New seperate testing document and snapshot

---
+++ client/src/components/MatchList.test.js
@@ -0,0 +1,16 @@
+import MatchList from "./MatchList";
+import renderer from 'react-test-renderer';
+import React from 'react';
+
+import {shallow, mount} from "enzyme";
+import { configure } from 'enzyme';
+
+
+describe("Match List Tests",function() {
+	test("Should render MatchList correctly",() => {
+		let page = renderer.create(<MatchList />)
+		expect(page.toJSON()).toMatchSnapshot();
+	})
+
+	
+})
