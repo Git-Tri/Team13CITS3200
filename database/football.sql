52448d26e71257bad4a8f52d8a50f54267291795
Ryan Oakley <seriously.online.email@gmail.com>
2019-09-15 12:28:00 +0800

    Updated football.sql
    Made matchid deletes cascade until we know what behaviour the client wants

--- database/football.sql
+++ database/football.sql
@@ -38,8 +38,8 @@ CREATE TABLE `edit` (
   PRIMARY KEY (`editid`),
   KEY `sid` (`sid`),
   KEY `usid` (`usid`),
-  CONSTRAINT `edit_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `match` (`id`),
-  CONSTRAINT `edit_ibfk_3` FOREIGN KEY (`usid`) REFERENCES `unstructured_data` (`usid`)
+  CONSTRAINT `edit_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `match` (`id`) ON DELETE CASCADE ,
+  CONSTRAINT `edit_ibfk_3` FOREIGN KEY (`usid`) REFERENCES `unstructured_data` (`usid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -120,6 +120,7 @@ CREATE TABLE `unstructured_data` (
   PRIMARY KEY (`usid`),
   KEY `matchid` (`matchid`),
   CONSTRAINT `unstructured_data_ibfk_1` FOREIGN KEY (`matchid`) REFERENCES `match` (`id`)
+	ON DELETE CASCADE	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
