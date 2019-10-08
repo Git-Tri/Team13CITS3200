2e53d001f333febcf0cf29bb00204f80b46029a9
Alex Burns <alexanderburns11@yahoo.com>
2019-10-08 19:02:50 +0800

    Added login route skeleton and fixed bcrypt dependancy

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const middleware = require("../middleware.js");
 const errorHandler = require("./errorHandler");
+const bcrypt = require('bcryptjs');
+const domain = require("../domain");
 
 
 exports.createRoutes = function(app) {
@@ -7,13 +9,17 @@ exports.createRoutes = function(app) {
     //Route to get all competitions (GET)
     middleware.post(app, "/login", (req, res) => {
 
-        cache.getAllComps((result => {
+        let username = req.body.username;
+        let password = req.body.password;
+        
+        if (username === null || password === null) {
+            console.log("Invalid login request");
+            res.sendStatus(400);
+        } else {
 
-            res.setHeader("Content-Type", "application/json");
-            res.send(JSON.stringify(result));
+        }
 
-
-        }), (err) => errorHandler.standard(err, res), (err) => errorHandler.standard(err, req));
+        
 
     });
 
