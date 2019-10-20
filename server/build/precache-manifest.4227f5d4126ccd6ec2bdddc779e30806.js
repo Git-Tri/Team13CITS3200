64192f3095113f3be3ef86ef7684cdb8e9da51b9
Ryan Oakley <seriously.online.email@gmail.com>
2019-10-21 01:13:08 +0800

    Added build folder to server

---
+++ server/build/precache-manifest.4227f5d4126ccd6ec2bdddc779e30806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca51a9f38f198786d1781e8538f343ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f22e25f30d7a94e91e",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "db2a67259afe1a6e953b",
+    "url": "/static/js/2.b90a7b78.chunk.js"
+  },
+  {
+    "revision": "a6f22e25f30d7a94e91e",
+    "url": "/static/js/main.bd411617.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
